e6f4d116a26d2a68804acba60e51d174a5153ce3
dheerajreddy <54369103+dheerajreddy01@users.noreply.github.com>
2023-01-25 16:43:44 -0600

    Update numpy_example.py

--- Assignment-3/numpy_example.py
+++ Assignment-3/numpy_example.py
@@ -4,10 +4,12 @@
 # (you can NOT implement it via for loop)
 
 
-import numpy as np 
+import numpy as np #importing the numpy module
 
-sampl=np.random.uniform(low=1,high=20,size=20)
+sampl=np.random.uniform(low=1,high=20,size=20) #generating the random float values between 1 to 20
 
-reshape_arr=sampl.reshape(4,5)
+reshape_arr=sampl.reshape(4,5) #reshaping the vector to 4*5 dimension
+
+#replacing the max values with 0s in each row 
 
 np.where(reshape_arr== np.max(reshape_arr, axis=1, keepdims=True), 0, reshape_arr)
